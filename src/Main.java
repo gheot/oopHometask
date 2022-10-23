e933561d17d2472696d118cc9518a6647b281bc6
nilova <marina.nilova@yandex.ru>
2022-10-23 17:08:27 +0300

    First part

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Human maksim = new Human(1987,"Максим", "Минск", " бренд-менеджер");
+        Human anna = new Human(1993,"Аня", "Москва", "методист образовательных программ");
+        Human katya = new Human(1994,"Катя", "Калининград", "продакт-менеджер");
+        Human artem = new Human(1995,"Артём", "Москва", "директор по развитию бизнеса");
+        maksim.humanInfo();
+        anna.humanInfo();
+        katya.humanInfo();
+        artem.humanInfo();
+        System.out.println();
     }
 }
 
